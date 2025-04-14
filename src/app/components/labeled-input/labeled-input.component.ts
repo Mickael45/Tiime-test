a271476897c2304d3ecb66a997eebfd192af06eb
Mickael <mickaelgomesconsulting@gmail.com>
2025-04-14 14:32:23 +0200

    Adds project structure and tooling

---
+++ src/app/components/labeled-input/labeled-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labeled-input',
+  imports: [],
+  templateUrl: './labeled-input.component.html',
+  styleUrl: './labeled-input.component.css'
+})
+export class LabeledInputComponent {
+
+}
---
+++ src/app/components/user-form/basic-info-form/basic-info-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-info-form',
+  imports: [],
+  templateUrl: './basic-info-form.component.html',
+  styleUrl: './basic-info-form.component.css'
+})
+export class BasicInfoFormComponent {
+
+}
