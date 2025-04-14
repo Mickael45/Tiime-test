61ab7e1b67865dc8f9fe292e6a2602517f22066f
Mickael <mickaelgomesconsulting@gmail.com>
2025-04-14 16:17:33 +0200

    Implements basic info form

--- src/app/components/user-form/basic-info-form/basic-info-form.component.ts
+++ src/app/components/user-form/basic-info-form/basic-info-form.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-info-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './basic-info-form.component.html',
 })
-export class BasicInfoFormComponent {}
+export class BasicInfoFormComponent {
+  @Input({ required: true }) form!: FormGroup<{
+    name: FormControl<string | null>;
+    email: FormControl<string | null>;
+    username: FormControl<string | null>;
+    phone: FormControl<string | null>;
+    website: FormControl<string | null>;
+  }>;
+}
--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,8 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { BasicInfoFormComponent } from './basic-info-form/basic-info-form.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [BasicInfoFormComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './user-form.component.html',
 })
-export class UserFormComponent {}
+export class UserFormComponent {
+  userForm = new FormGroup({
+    basicInfo: new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl(''),
+      username: new FormControl(''),
+      phone: new FormControl(''),
+      website: new FormControl(''),
+    }),
+    address: new FormGroup({
+      street: new FormControl(''),
+      suite: new FormControl(''),
+      city: new FormControl(''),
+      zipcode: new FormControl(''),
+      geo: new FormGroup({
+        lat: new FormControl(''),
+        lng: new FormControl(''),
+      }),
+    }),
+    company: new FormGroup({
+      name: new FormControl(''),
+      catchPhrase: new FormControl(''),
+      bs: new FormControl(''),
+    }),
+  });
+}
