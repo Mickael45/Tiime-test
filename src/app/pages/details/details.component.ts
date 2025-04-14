5bcccb7a171b1eed9ceb307bc36a63bf97849684
Mickael <mickaelgomesconsulting@gmail.com>
2025-04-14 14:40:55 +0200

    Adds home route

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -4,8 +4,5 @@ import { Component } from '@angular/core';
   selector: 'app-details',
   imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
 })
-export class DetailsComponent {
-
-}
+export class DetailsComponent {}
